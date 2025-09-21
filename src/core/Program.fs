0f719d790e991f3364de8b5ffc0ceb63122021ec
lif0 <22912194+lif0@users.noreply.github.com>
2025-09-21 19:16:08 +0300

    WFC: add project core;

---
+++ src/core/Program.fs
@@ -0,0 +1,12 @@
+﻿namespace Core
+
+module WordStatistic =
+    type Word = { Word: string; Freq: uint32 }
+    let toWord word = { Word = word; Freq = 0u }
+    
+    let Build (wordList: string list) (fnormalize: string -> string) =
+        wordList
+        |> List.map fnormalize
+        |> List.groupBy id
+        |> List.map (fun (word, group) -> { Word = word; Freq = uint32 group.Length })
+        |> List.sortByDescending (fun word -> word.Freq)
